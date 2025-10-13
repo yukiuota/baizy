78e6a281ee55308b2ba1ae618694dde8cd5be272
uota <shiratuki0921@gmail.com>
2025-10-13 22:03:28 +0900

    ヘッダーのタグ出力を整理し、条件分岐を簡素化

--- header.php
+++ header.php
@@ -1,18 +1,14 @@
 <?php if ( ! defined( 'ABSPATH' ) ) exit; ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<?php if ( is_home() || is_front_page() ) : ?>
+<?php $head_tag = (is_home() || is_front_page()) ? 'website' : 'article'; ?>
 
-<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
-    <?php else : ?>
-
-    <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
-        <?php endif; ?>
-        <?php baizy_template_part( 'public/include/tags/head_top' ); // head_tag ?>
-        <meta charset="<?php bloginfo( 'charset' ); ?>">
-        <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0" />
-        <?php wp_head(); ?>
-    </head>
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# <?php echo $head_tag; ?>: http://ogp.me/ns/<?php echo $head_tag; ?>#">
+    <?php baizy_template_part( 'public/include/tags/head_top' ); // head_tag ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0" />
+    <?php wp_head(); ?>
+</head>
 
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
