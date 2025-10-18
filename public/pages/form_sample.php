78d0b94ee022129291a56afe75c3c805447fde9e
uota <shiratuki0921@gmail.com>
2025-10-19 01:55:31 +0900

    Contactform7で使用するサンプルコードの追加

---
+++ public/pages/form_sample.php
@@ -0,0 +1,124 @@
+<div class="c-form">
+    <div class="c-form__item">
+        氏名<br>
+        [text* your-name id:your-name autocomplete:name]
+    </div>
+    <br><br>
+
+    <div class="c-form__item">
+        メールアドレス<br>
+        [email* your-email id:your-email autocomplete:email]
+    </div>
+    <br><br>
+
+    <div class="c-form__item">
+        URL<br>
+        [url* url id:url]
+    </div>
+    <br><br>
+
+    <div class="c-form__item">
+        電話番号<br>
+        [tel* tel id:tel]
+    </div>
+    <br><br>
+
+    <div class="c-form__item">
+        数値<br>
+        [number* number id:number]
+    </div>
+    <br><br>
+
+    <div class="c-form__item">
+        日付<br>
+        [date* date id:date]
+    </div>
+    <br><br>
+
+    <div class="c-form__item">
+        テキストエリア<br>
+        [textarea* your-message id:your-message]
+    </div>
+    <br><br>
+
+    <div class="c-form__item">
+        ドロップダウンメニュー<br>
+        [select* select id:select "選択肢 1" "選択肢 2" "選択肢 3"]
+    </div>
+    <br><br>
+
+    <div class="c-form__item">
+        チェックボックス<br>
+        [checkbox checkbox id:checkbox "選択肢 1" "選択肢 2" "選択肢 3"]
+    </div>
+    <br><br>
+
+    <div class="c-form__item">
+        ラジオボタン<br>
+        [radio radio id:radio use_label_element "選択肢 1" "選択肢 2" "選択肢 3"]
+    </div>
+    <br><br>
+    <input type="button" class="confirm_button c-form-confirm_button" value="内容を確認する">
+</div>
+
+
+<br><br><br>
+
+<!-- 確認画面 -->
+<div class="form c-form-confirm">
+
+    <div class="c-form__item">1
+        <div class="c-form-confirm_your-name"></div>
+    </div>
+    <br><br>
+
+    <div class="c-form__item">2
+        <div class="c-form-confirm_your-email"></div>
+    </div>
+    <br><br>
+
+    <div class="c-form__item">3
+        <div class="c-form-confirm_url"></div>
+    </div>
+    <br><br>
+
+    <div class="c-form__item">4
+        <div class="c-form-confirm_tel"></div>
+    </div>
+    <br><br>
+
+    <div class="c-form__item">5
+        <div class="c-form-confirm_number"></div>
+    </div>
+    <br><br>
+
+    <div class="c-form__item">6
+        <div class="c-form-confirm_date"></div>
+    </div>
+    <br><br>
+
+    <div class="c-form__item">7
+        <div class="c-form-confirm_your-message"></div>
+    </div>
+    <br><br>
+
+    <div class="c-form__item">8
+        <div class="c-form-confirm_select"></div>
+    </div>
+    <br><br>
+
+    <div class="c-form__item">9
+        <div class="c-form-confirm_checkbox"></div>
+    </div>
+    <br><br>
+
+    <div class="c-form__item">10
+        <div class="c-form-confirm_radio"></div>
+    </div>
+    <br><br>
+
+
+    <input type="button" class="back_button" value="修正する">
+    <br>
+    [submit "送信"]
+</div>
