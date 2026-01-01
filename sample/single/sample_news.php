0ed71cb6088b7e25c72ae85da4f26b4009a2936f
uota <uota1921@gmail.com>
2026-01-01 15:06:50 +0900

    コメント表示機能を追加し、カスタムテキストの表示部分のインデントを修正しました。

--- sample/single/sample_news.php
+++ sample/single/sample_news.php
@@ -8,10 +8,16 @@ require_once get_template_directory() . '/app/controller/news.php';
 <main id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
     <?php if ( $custom_text ) : ?>
-        <div class="custom-text">
-            <?php echo '<p>' . esc_html( $custom_text ) . '</p>'; ?>
-        </div>
+    <div class="custom-text">
+        <?php echo '<p>' . esc_html( $custom_text ) . '</p>'; ?>
+    </div>
     <?php endif; ?>
+
+    <?php // コメント表示
+    if ( comments_open() || get_comments_number() ) {
+        comments_template();
+    }
+    ?>
 
     <?php endwhile; ?>
 </main>
