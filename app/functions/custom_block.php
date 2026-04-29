a72e179f887ff6571f94e15426c5b8aa27fbd576
uota <shiratuki0921@gmail.com>
2026-04-29 16:28:57 +0900

    refactor: 不要な見出しカスタマイズ関数を削除

--- app/functions/custom_block.php
+++ app/functions/custom_block.php
@@ -113,59 +113,6 @@ add_filter( 'allowed_block_types_all', 'restrict_blocks_for_cases', 10, 2 );
 
 
 
-
-// -----------------------------------------------------
-// ブロックエディタの見出しHTMLをカスタマイズ
-// -----------------------------------------------------
-function custom_heading_html( $block_content, $block ) {
-	// 見出しブロックの場合のみ処理
-	if ( 'core/heading' !== $block['blockName'] ) {
-		return $block_content;
-	}
-
-	// 例: 見出しに特定のクラスやデザイン要素を追加
-	$modified_content = str_replace(
-		array( '<h1', '<h2', '<h3', '<h4', '<h5', '<h6' ),
-		array(
-			'<h1 class="post-heading01"',
-			'<h2 class="post-heading02"',
-			'<h3 class="post-heading03"',
-			'<h4 class="post-heading04"',
-			'<h5 class="post-heading05"',
-			'<h6 class="post-heading06"',
-		),
-		$block_content
-	);
-
-	return $modified_content;
-}
-add_filter( 'render_block', 'custom_heading_html', 10, 2 );
-
-
-
-
-
-// -----------------------------------------------------
-// 管理画面のGutenberg上のh1~h6にだけクラスを追加
-// -----------------------------------------------------
-add_filter(
-	'render_block',
-	function ( $block_content, $block ) {
-		// 管理画面でのみ実行（REST APIリクエストも含む）
-		if ( ( is_admin() || wp_doing_ajax() || defined( 'REST_REQUEST' ) ) && 'core/heading' === $block['blockName'] ) {
-			$block_content = preg_replace(
-				'/<h([1-6])([^>]*)>/',
-				'<h$1 class="post-heading0$1"$2>',
-				$block_content
-			);
-		}
-		return $block_content;
-	},
-	10,
-	2
-);
-
-
 // Gutenbergエディター内でのブロック属性を変更
 function modify_heading_block_attributes() {
 	?>
